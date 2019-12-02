056760589b59ce7973dff01423e907f315f2a10c
VT-johnkl <johnk.lec@gmail.com>
2019-12-01 19:26:52 -0500

    Day 1 solution

---
+++ Paths_aoc2019.hs
@@ -0,0 +1,4 @@
+module Paths_aoc2019 where
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName = return
---
+++ src/Day1.hs
@@ -0,0 +1,32 @@
+module Day1 where
+
+import Text.Read (readMaybe)
+import Paths_aoc2019
+
+-- Part 1 Fuel Calculation
+findFuel :: Maybe Int -> Int
+findFuel Nothing = 0
+findFuel (Just mass)
+    | mass < 9  = 0     -- If fuel would be negative just return 0
+    | otherwise = mass `div` 3 - 2
+
+-- Part 2 Fuel Calculation
+findFuel' :: Maybe Int -> Int
+findFuel' Nothing = 0
+findFuel' (Just mass)
+    | mass < 9 = 0
+    | otherwise = fuel + findFuel' (Just fuel)
+    where
+        fuel = mass `div` 3 - 2
+
+execute = do
+    filePath <- getDataFileName "inputs/day1-input.txt"
+    massesFile <- readFile filePath
+    let massesFileSplit = lines massesFile
+    let masses = map readMaybe massesFileSplit  -- [Just x, Just y, ..., Just z]
+    let fuels = map findFuel masses   -- Part 1 Calculation
+    let fuels' = map findFuel' masses  -- Part 2 Calculation
+    print ("Part 1:")
+    print (sum fuels)
+    print ("Part 2:")
+    print (sum fuels')
